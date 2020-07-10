0654fe420ea71fc74d3647d8143bbef06091c2eb
Chaitanya Eranki <chaitanyaeranki@y7mail.com>
2020-07-10 14:22:55 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5 + 5 - 6 * 24 / 24;
+        System.out.println(x);
+        System.out.println(23 % 4);
+    }
+}
